31cd313c1d3f60019e189053a60004e3dcefd053
Sasha2009MIIT <90352219+BMyGf@users.noreply.github.com>
2021-12-30 13:14:25 +0000

    Задание 2-1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <windows.h> //добавил данную библиотеку для взаимодействия с ОС
 
 using namespace std;
 
@@ -9,36 +10,64 @@ using namespace std;
 * \brief Математическая функция, рассчитывающая объём шара
 * \radius - радиус шара
 **/
-double getVolume(const double r);
+enum class Sphere
+{
+	/**
+	 * \brief Объём шара.
+	 */
+	Volume,
+
+	/**
+	 * \brief Площадь поверхности шара.
+	 */
+	 SurfaceArea
+};
+double getVolume(const double radius);
 
 /**
 * \brief Математическая функция, рассчитывающая площадь поверхности шара
 * \radius - радиус шара
 **/
-double getSurfaceArea(const double r);
+double getSurfaceArea(const double radius);
 
 /**
  * \brief Точка входа в программу.
  */
-int main() 
+int main()
 {
-    double radius;
+	SetConsoleCP(1251); //использую данную команду для отображения кириллицы в консоли
+	SetConsoleOutputCP(1251); //использую данную команду для отображения кириллицы в консоли
+	{
+		cout << "Выберите то, что вы бы хотели узнать"
+			<< "\n Объём - " << static_cast<int>(Sphere::Volume)
+			<< "\n Площадь поверхности - " << static_cast<int>(Sphere::SurfaceArea);
+		cout << endl;
+		int sphere;
+		cin >> sphere;
+		double radius = 0.0;
+		cout << "Введите radius ";
+		cin >> radius;
 
-    cout << "Введите радиус и нажмите Enter: \n";
-    cin >> radius;
+		const auto Figure = static_cast<Sphere>(sphere);
+		switch (Figure)
+		{
+		case Sphere::Volume:
+			cout << "V = " << getVolume(radius);
+			break;
+		case Sphere::SurfaceArea:
+			cout << "S = " << getSurfaceArea(radius);
+			break;
+			return 0;
+		}
+	}
 
-    const auto Volume = getVolume(radius);
-    const auto SurfaceArea = getSurfaceArea(radius);
-
-    cout << "Радиус radius = " << radius << "\n Объём шара = " << Volume << "\n Площадь поверхности шара = " << SurfaceArea;
-    return 0;
 }
 
-double getVolume(const double radius) 
+double getVolume(const double radius)
 {
-    return pow(radius, 3) * ((4 * M_PI) / 3);
+	return pow(radius, 3) * ((4 * M_PI) / 3);
 }
 double getSurfaceArea(const double radius)
 {
-    return pow(radius, 2) * M_PI * 4;
+	return pow(radius, 2) * M_PI * 4;
 }
